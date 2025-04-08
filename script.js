3ecc4a8741658ad2743babf76de927a382124cb0
jamesowusugithub <owusujamess883@gmail.com>
2025-04-08 18:36:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// what fns do we want in the website
+// one to have animations when the form is submitted
+// one to make the folios pop up when clicked on 
+// also one to make the folios do something when hovered over
+// one to make the answers to the faq pop up when clicked on 
+//  rememeber that we want to be impressionable to impressionable teens so the animations should be modern
+// also animations over the whole site when scrollint 
