6958a9fa66baf58203f5256c660f4d42745feaae
jamesowusugithub <owusujamess883@gmail.com>
2025-04-10 18:12:29 +0100

    Co-authored-by: Ayansud28 <Ayansud28@users.noreply.github.com> Co-authored-by: dgrimley2001@gmail.com <dgrimley2001@gmail.com>

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// imports 
-// what fns do we want in the website
-
-// one to have animations when the form is submitted
-// one to make the folios pop up when clicked on ⬅️ this 
-// also one to make the folios do something when hovered over
+// one to have animations when the form is submitted ☑️
+// one to make the folios pop up when clicked on  ☑️
+// also one to make the folios do something when hovered over ✔️
 // one to make the answers to the faq pop up when clicked on ☑️
-// also animations over the whole site when scrolling ☑️ ❌
+// also animations over the whole site when scrolling ☑️ 
 
 
 document.addEventListener('DOMContentLoaded', function () {
   /* faq js */
     const accordions = document.querySelectorAll('.accordion');
-  
+  // click faq question, answer pops up
     accordions.forEach(button => {
       button.addEventListener('click', function () {
         this.classList.toggle('active');
@@ -29,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function () {
   /* END FAQ JS */
 
 // ========== gsap stuff ==============//
+// when u scroll the elements lag to enter the page giving u the animation.
   gsap.registerPlugin(ScrollTrigger);
   gsap.utils.toArray(".fade-in").forEach(element => {
     gsap.from(element, {
@@ -42,72 +40,55 @@ document.addEventListener('DOMContentLoaded', function () {
       duration: 0.89
     });
   });
-                        
-
-// The function we need there minimum is one to make the cards he made "popup"
-// so how would we make it? 
-// okay, the first one would be a listener for when the card is clicked
-// we would need 2 variables, 1 as the getter for the card 
-// 1 for the actual popup. 
-// Then we would have an if statement that asks if the card has popped up ]
-// (second variable) the we make it active (show the popup)
-
-// the other is when the close button is clicked it should close the popup
-// but first the popup function should work before we worry abt that
 
 //=== Portfolio page === //
-    const openButtons = document.querySelectorAll('[data-popup-target');
+    const openButtons = document.querySelectorAll('[data-popup-target]');
     const closebuttons = document.querySelectorAll('[data-close]');
-
-
+    
 // open popup
   openButtons.forEach(button => {
     button.addEventListener('click', () => {
-      const popupid = button.getAttribute('data-popup[-target');
-      const popup = document.querySelector(popupid);
-      popup.classList.add('active');
-    })
-  })
+      const popupId = button.getAttribute('data-popup-target');
+      const popup = document.querySelector(popupId);
+      console.log("Working for: ", popupId)
+      if (popup){
+        popup.classList.add('active');
+      }
+    });
+  });
 
 // close popup
   closebuttons.forEach(button => {
     button.addEventListener('click', () => {
       const popup = button.closest('.popup-card');
       popup.classList.remove('active');
-    })
-
-  
-});  
-
-// end of folio page
-
-
-// content page 
-    
-
+    });
+  });  
+// end of folio page //
 });
-  
-  
-  
 
+// contact page 
+const form = document.getElementById("contact_form")
 
+form.addEventListener("submit", function(element) { 
+  // only works when everything is filled in
+  element.preventDefault();
 
-
-// for contact page 
-// field validation - do not need because requierd in html does it for me 
-// success animation - should be easy just import the library, whats it called
-// variable grab 
-// const mainElem = document.querySelectorAll('main');
-
-// ScrollSmoother.create({
-//     smooth: 1,
-//     effects: true,
-//     speed: 0.1
-// })
-// ScrollTrigger.create({
-//     trigger: mainElem[0], 
-//     start: "top center",
-//     end: "bottom top",
-//     toggleActions: play, none, none,reverse
-// })
-// ScrollTrigger.normalizeScroll(true);
+  // simple toast from sweetalert  
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
+  });
+  Toast.fire({
+    icon: "success",
+    title: "Form Submitted! We'll get back to you ASAP"
+  }); 
+  form.reset()
+}); 
